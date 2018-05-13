02ee96935d56f6bc8e52a2cd69b63076e3cedacc
Aleksey Konotop <alekseykonotop@gmail.com>
2018-05-13 23:51:48 +0300

    Написал 7 из 8 функций.

--- hw_1.4.py
+++ hw_1.4.py
@@ -24,12 +24,24 @@ directories = {
 # as – add shelf – команда, которая спросит номер новой полки и добавит ее в перечень;
 
 
+def input_doc_number():
+    """Функция запрашивает номер документа и
+    возвращает его в виде строки.
+    """
+
+    doc_num = input('Введите номер документа: ')
+    return doc_num
+
 def get_name_person():
     """Функция спрашивает номер документа и
     выводит имя человека, которому он принадлежит.
     """
 
-    print('Функция находится в разработке.')
+    # print('Функция находится в разработке.')
+    doc_number = input_doc_number()
+    for doc in documents:
+        if doc['number'] == doc_number:
+            print('Владельцем документа под номером "{0}" является {1}.'.format(doc_number, doc['name']))
 
 
 def get_all_doc_list():
@@ -37,7 +49,9 @@ def get_all_doc_list():
     в формате passport "2207 876234" "Василий Гупкин".
     """
 
-    print('Функция находится в разработке.')
+    print('Список всех документов:')
+    for doc in documents:
+        print('{0} "{1}" {2}'.format(doc['type'], doc['number'], doc['name']))
 
 
 def get_shelf_number():
@@ -45,8 +59,11 @@ def get_shelf_number():
     выведет номер полки, на которой он находится.
     """
 
-    print('Функция находится в разработке.')
-
+    # print('Функция находится в разработке.')  # Удалить
+    doc_number = input_doc_number()
+    for number_shelf, doc_numbers in directories.items():
+        if doc_number in doc_numbers:
+            print('Документ номер "{0}" хранится на {1} полке.'.format(doc_number, number_shelf))
 
 def add_new_doc():
     """Функция добавит новый документ в каталог и в
@@ -54,16 +71,53 @@ def add_new_doc():
     имя владельца и номер полки, на котором
     он будет храниться.
     """
-    print('Функция находится в разработке.')
+    # print('Функция находится в разработке.')  # Удалить
+    new_doc = input('Введите данные через запятую без пробеллов в порядке: номер, тип, имя владельца и номер полки хранения').split(',')
+    documents.append(dict([("type", new_doc[1]), ('number', new_doc[0]), ('name', new_doc[2])]))
+    print('Актульный список документов: ', documents)
+    for number_shelf, doc_numbers in directories.items():
+        if number_shelf == new_doc[3]:
+            doc_numbers.append(new_doc[0])
+    print('Актуальный список полок: ', directories)
 
 
+# В функции del_doc и move_doc вынести в отдельную функцию получение индекса документа в списке documents для последующего удаления.
+# Так же вынести в отд. функ. получение номера полки для последующего удаления документа с этой полки.
 def del_doc():
     """Функция спросит номер документа и
     удалит его из каталога и из перечня полок.
     """
 
     print('Функция находится в разработке.')
+    global documents
+    doc_number = input_doc_number()
+    # Удалим документ из общего списка
+    ind = 0
+    for i, doc in enumerate(documents):
+        if doc['number'] == doc_number:
+            ind = i
+            break
+    documents.pop(ind)  # Удалили документ из общего списка
+    print('Обновленный список документов:', documents)
 
+    # Удалим документ с полки
+    for number_shelf, doc_numbers in directories.items():
+        if doc_number in doc_numbers:
+            break
+    print('Полка {0}, документы на полке {1}'.format(number_shelf, doc_numbers))
+    for i, doc_numb in enumerate(directories[number_shelf]):
+        if doc_numb == doc_number:
+            print('Мы нашли нужный документ под номером: {0}'.format(i))
+            break
+    del directories[number_shelf][i]
+    print('Теперь directories выглядит так: ', directories)
+
+
+
+
+
+
+# del_doc()
 
 def move_doc():
     """Функция спросит номер документа и
@@ -78,7 +132,15 @@ def add_new_shelf():
     добавит ее в перечень.
     """
 
-    print('Функция находится в разработке.')
+    global directories
+    shelf_number = input('Введите номер новой полки: ')
+    if shelf_number not in directories:
+        directories[shelf_number] = []
+        print('Добавлена новая полка. Актуальный список полок: {}'.format(directories))
+    else:
+        print('Полка с таким номером уже есть, задайте другой номер.')
+
+# add_new_shelf()
 
 
 
@@ -117,4 +179,4 @@ def run():
             break
 
           
-run()
+# run()
